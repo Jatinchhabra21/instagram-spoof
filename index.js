73fe71acaacb91daec590d4c9648a37724e3fe72
Jatin Chhabra <cjatin2822@gmail.com>
2022-11-11 16:31:05 +0530

    added data to new separate js file

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-let posts = [
-    {
-      name: "Vincent van Gogh",
-      username: "vincey1853",
-      location: "Zundert, Netherlands",
-      avatar: "images/avatar-vangogh.jpg",
-      post: "images/post-vangogh.jpg",
-      comment: "just took a few mushrooms lol",
-      likes: 21,
-      hasUserLiked: false,
-    },
-    {
-      name: "Gustave Courbet",
-      username: "gus1819",
-      location: "Ornans, France",
-      avatar: "images/avatar-courbet.jpg",
-      post: "images/post-courbet.jpg",
-      comment: "i'm feelin a bit stressed tbh",
-      likes: 4,
-      hasUserLiked: false,
-    },
-    {
-      name: "Joseph Ducreux",
-      username: "jd1735",
-      location: "Paris, France",
-      avatar: "images/avatar-ducreux.jpg",
-      post: "images/post-ducreux.jpg",
-      comment:
-        "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-      likes: 152,
-      hasUserLiked: false,
-    },
-  ];
+import { posts } from './data.js';
   
   const postContainer = document.getElementById("posts");
   const post = (posts) => {
